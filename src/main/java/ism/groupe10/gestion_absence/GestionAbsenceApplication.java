d4f1ece57a58a7b533ccf46886afae1a2b99f5ab
ouzeon007 <nousseynou20@gmail.com>
2025-05-26 23:09:16 +0000

    Initial commit

---
+++ src/main/java/ism/groupe10/gestion_absence/GestionAbsenceApplication.java
@@ -0,0 +1,13 @@
+package ism.groupe10.gestion_absence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAbsenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionAbsenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ism/groupe10/gestion_absence/GestionAbsenceApplicationTests.java
@@ -0,0 +1,13 @@
+package ism.groupe10.gestion_absence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAbsenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
