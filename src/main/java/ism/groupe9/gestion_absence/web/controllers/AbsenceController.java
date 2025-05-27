d4c55e97d1e8282318aeb2db340f9cad3ed0e419
ouzeon007 <nousseynou20@gmail.com>
2025-05-27 12:52:24 +0000

    Initial commit

--- src/main/java/ism/groupe9/gestion_absence/web/controllers/AbsenceController.java
+++ src/main/java/ism/groupe9/gestion_absence/web/controllers/AbsenceController.java
@@ -1,26 +1,26 @@
-// package ism.groupe9.gestion_absence.web.controllers;
+package ism.groupe9.gestion_absence.web.controllers;
 
-// import java.util.Map;
+import java.util.Map;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// import ism.groupe9.gestion_absence.data.entities.Absence;
+import ism.groupe9.gestion_absence.data.entities.Absence;
 
-// @RequestMapping("/api/absence")
-// public interface AbsenceController {
+@RequestMapping("/api/absence")
+public interface AbsenceController {
 
-//   @GetMapping("")
-//   ResponseEntity<Map<String, Object>> getAll();
+  @GetMapping("")
+  ResponseEntity<Map<String, Object>> getAll();
 
-//   @GetMapping("/{id}")
-//   ResponseEntity<Map<String, Object>> getByEtudiantId(@PathVariable String id);
+  @GetMapping("/{id}")
+  ResponseEntity<Map<String, Object>> getByEtudiantId(@PathVariable String id);
 
-//   @PostMapping
-//   ResponseEntity<Map<String, Object>> create(@RequestBody Absence absence);
+  @PostMapping
+  ResponseEntity<Map<String, Object>> create(@RequestBody Absence absence);
 
-// }
+}
--- src/main/java/ism/groupe9/gestion_absence/web/controllers/EtudiantController.java
+++ src/main/java/ism/groupe9/gestion_absence/web/controllers/EtudiantController.java
@@ -1,33 +1,33 @@
-// package ism.groupe9.gestion_absence.web.controllers;
+package ism.groupe9.gestion_absence.web.controllers;
 
-// import java.util.Map;
+import java.util.Map;
 
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// import ism.groupe9.gestion_absence.data.entities.Etudiant;
+import ism.groupe9.gestion_absence.data.entities.Etudiant;
 
-// @RequestMapping("/api/etudiant")
-// public interface EtudiantController {
+@RequestMapping("/api/etudiant")
+public interface EtudiantController {
   
-//   @GetMapping("")
-//   ResponseEntity<Map<String, Object>> getAll();
+  @GetMapping("")
+  ResponseEntity<Map<String, Object>> getAll();
 
-//   @GetMapping("/{id}")
-//   ResponseEntity<Map<String, Object>> getById(@PathVariable String id);
+  @GetMapping("/{id}")
+  ResponseEntity<Map<String, Object>> getById(@PathVariable String id);
 
-//   @PostMapping
-//   ResponseEntity<Map<String, Object>> create(@RequestBody Etudiant etudiant);
+  @PostMapping
+  ResponseEntity<Map<String, Object>> create(@RequestBody Etudiant etudiant);
 
-//   @PutMapping("/{id}")
-//   ResponseEntity<Map<String, Object>> update(@PathVariable String id, @RequestBody Etudiant etudiant);
+  @PutMapping("/{id}")
+  ResponseEntity<Map<String, Object>> update(@PathVariable String id, @RequestBody Etudiant etudiant);
 
-//   @DeleteMapping("/{id}")
-//   ResponseEntity<Map<String, Object>> delete(@PathVariable String id);
-// }
+  @DeleteMapping("/{id}")
+  ResponseEntity<Map<String, Object>> delete(@PathVariable String id);
+}
