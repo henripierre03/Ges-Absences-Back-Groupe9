8b9daef2195fecacd4a8dcde4c0b0edbc2750983
Ameth Ba <amethba8826@gmail.com>
2025-05-27 22:34:03 +0000

    `initialcommit`

--- src/main/java/ism/groupe9/gestion_absence/data/entities/Etudiant.java
+++ src/main/java/ism/groupe9/gestion_absence/data/entities/Etudiant.java
@@ -1,8 +1,6 @@
 package ism.groupe9.gestion_absence.data.entities;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
 
 import lombok.Getter;
 import lombok.Setter;
