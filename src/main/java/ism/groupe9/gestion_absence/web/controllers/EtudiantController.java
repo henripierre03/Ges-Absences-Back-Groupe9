800f949676a227e1b7583acc38601af209a0eb47
ouzeon007 <nousseynou20@gmail.com>
2025-05-28 11:42:49 +0000

    Ajout de la documentation swagger

--- src/main/java/ism/groupe9/gestion_absence/web/controllers/EtudiantController.java
+++ src/main/java/ism/groupe9/gestion_absence/web/controllers/EtudiantController.java
@@ -11,18 +11,28 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import ism.groupe9.gestion_absence.data.entities.Etudiant;
 
 @RequestMapping("/api/etudiant")
 public interface EtudiantController {
 
   @GetMapping("")
+  @Operation(summary = "Recuperer tous les étudiants", description = "Cette méthode permet de récupérer la liste de tous les étudiants.")
+  @ApiResponse(responseCode = "200", description = "Liste des étudiants récupérée avec succès.")
+  @ApiResponse(responseCode = "404", description = "Aucun étudiant trouvé.")
   ResponseEntity<Map<String, Object>> getAll();
 
   @GetMapping("/{matricule}")
-  ResponseEntity<Map<String, Object>> getByEtudiant(@PathVariable String matricule);
+  ResponseEntity<Map<String, Object>> getByEtudiant(@Parameter(description = "Matricule de l'etudiant",required = true) @PathVariable String matricule);
 
   @PostMapping
+  @Operation(summary = "Créer un nouvel étudiant", description = "Cette méthode permet de créer un nouvel étudiant.")
+  @ApiResponse(responseCode = "201", description = "Étudiant créé avec succès.")
+  @ApiResponse(responseCode = "400", description = "Requête invalide, les données fournies ne sont pas valides.")
+  @ApiResponse(responseCode = "409", description = "Conflit, l'étudiant existe déjà.")
   ResponseEntity<Map<String, Object>> create(@RequestBody Etudiant etudiant);
 
   @PutMapping("/{id}")
@@ -33,5 +43,5 @@ public interface EtudiantController {
 
   @GetMapping("/absence/{matricule}")
   ResponseEntity<Map<String, Object>> getAbsencesByEtuduiant(@PathVariable String matricule);
-  
+
 }
