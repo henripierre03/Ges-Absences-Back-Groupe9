1cd8a37eab508d02b1a3e6e3b0844babebe51b0b
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-05-29 12:48:19 +0000

    mis a jour de la generalisation

--- src/main/java/ism/groupe9/gestion_absence/data/entities/Etudiant.java
+++ src/main/java/ism/groupe9/gestion_absence/data/entities/Etudiant.java
@@ -3,12 +3,10 @@ package ism.groupe9.gestion_absence.data.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import ism.groupe9.gestion_absence.data.enums.Filiere;
 import ism.groupe9.gestion_absence.data.enums.Niveau;
-import ism.groupe9.gestion_absence.data.enums.UserRole;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,13 +16,6 @@ import lombok.Setter;
 @Document
 public class Etudiant extends User {
 
-  @Id
-  private String id;
-  private String nom;
-  private String prenom;
-  private String email;
-  private String password;
-  private UserRole role;
   private Filiere filiere;
   private String matricule;
   private Niveau niveau;
