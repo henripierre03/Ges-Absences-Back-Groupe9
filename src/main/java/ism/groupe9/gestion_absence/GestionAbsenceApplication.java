e869488da615fee645f733a98e26ad601827a803
Ameth Ba <amethba8826@gmail.com>
2025-06-01 10:29:06 +0000

    `add Auhtorize sur Swagger `

--- src/main/java/ism/groupe9/gestion_absence/GestionAbsenceApplication.java
+++ src/main/java/ism/groupe9/gestion_absence/GestionAbsenceApplication.java
@@ -9,5 +9,4 @@ public class GestionAbsenceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GestionAbsenceApplication.class, args);
 	}
-
 }
--- src/main/java/ism/groupe9/gestion_absence/web/controllers/JustificationController.java
+++ src/main/java/ism/groupe9/gestion_absence/web/controllers/JustificationController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +26,12 @@ public interface JustificationController {
   @Operation(summary = "Recuperer la justification par absence", description = "Cette méthode permet de récupérer la justification associée à une absence spécifique.")
   @ApiResponse(responseCode = "200", description = "Justification récupérée avec succès.")
   @ApiResponse(responseCode = "404", description = "Aucune justification trouvée pour l'absence spécifiée.")
-  ResponseEntity<Map<String, Object>> getByAbsenceId(String absenceId);
+  ResponseEntity<Map<String, Object>> getByAbsenceId(@PathVariable() String absenceId);
 
   @PutMapping("/{id}")
   @Operation(summary = "Valider la justification", description = "Cette méthode permet de valider une justification d'absence.")
   @ApiResponse(responseCode = "200", description = "Justification validée avec succès.")
   @ApiResponse(responseCode = "404", description = "Justification non trouvée ou déjà validée.")
   @ApiResponse(responseCode = "400", description = "Requête invalide, les données fournies ne sont pas valides.")
-  ResponseEntity<Map<String, Object>> validationJustification(String id, @RequestBody Justification justification);
+  ResponseEntity<Map<String, Object>> validationJustification(@PathVariable() String id, @RequestBody Justification justification);
 }
