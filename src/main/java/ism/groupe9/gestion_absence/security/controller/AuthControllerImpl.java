c1fd78f3896066490f377d2b36c8b6d631a73056
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-06-01 12:34:14 +0000

    Ajout du crossOrigin

--- src/main/java/ism/groupe9/gestion_absence/security/controller/AuthControllerImpl.java
+++ src/main/java/ism/groupe9/gestion_absence/security/controller/AuthControllerImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +25,8 @@ import ism.groupe9.gestion_absence.web.dto.response.RestResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthControllerImpl implements AuthController {
 
   private final AuthenticationManager authenticationManager;
