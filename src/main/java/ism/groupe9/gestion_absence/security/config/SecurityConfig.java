0c11a1234a1982e5f9c7eb2f4d0757fbc1620797
Ousseynou Ndiaye <nousseynou20@gmail.com>
2025-06-04 23:53:46 +0000

    ajout du versel.json

--- src/main/java/ism/groupe9/gestion_absence/security/config/SecurityConfig.java
+++ src/main/java/ism/groupe9/gestion_absence/security/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(
                 "/",
-                // "/api/qrcode/**",
+                "/api/qrcode/**",
                 "/api/auth/**",
                 "/api/mobile/**",
                 // "/api/web/**",
