35dfcc9293efc622ab5790023ffefd991ca8b543
Ameth Ba <amethba8826@gmail.com>
2025-06-04 01:54:36 +0000

    `add`

--- src/main/java/ism/groupe9/gestion_absence/mobile/dto/response/LoginResponse.java
+++ src/main/java/ism/groupe9/gestion_absence/mobile/dto/response/LoginResponse.java
@@ -1,5 +1,7 @@
 package ism.groupe9.gestion_absence.mobile.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import ism.groupe9.gestion_absence.data.enums.UserRole;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,6 +12,7 @@ import lombok.Setter;
 
 @AllArgsConstructor
 public class LoginResponse {
+  @JsonProperty("id")
   private String id;
   private String token;
   private String email;
